5880e11b03a9ef1bf2fe77c3b1f5b1fc929269d1
dev-raul <rraulsilva.dev@gmail.com>
2024-02-12 02:52:51 -0300

    fix(http): app controllers

---
+++ src/infra/http/controllers/app/app.controller.spec.ts
@@ -0,0 +1,23 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(appController.get()).toEqual({
+        message: 'Nest js template with prisma(pg), logger and http router',
+      });
+    });
+  });
+});
